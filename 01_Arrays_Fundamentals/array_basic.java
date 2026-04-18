d189ce8323f0f961f2a854f778dd03b40d2015bf
sammiazaz <SAMMIAZAZ2004@GMAIL.COM>
2026-04-19 00:45:07 +0530

    knd

---
+++ 01_Arrays_Fundamentals/array_basic.java
@@ -0,0 +1,6 @@
+public static void main(String[] args) {
+       // creating array 
+
+        int[] arr = new int[5]; // size = 5
+        System.out.println("Array created with size: " + arr.length);
+    }
