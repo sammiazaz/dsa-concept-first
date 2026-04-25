81532a32f381bda018260dd872b2dc86cc62451e
sammiazaz <sammiazaz2004@gmail.com>
2026-04-25 22:14:07 +0530

    2ns question

--- 01_Arrays_Fundamentals/PQ.java
+++ 01_Arrays_Fundamentals/PQ.java
@@ -1,15 +1,26 @@
 public class PQ {
-    //****************question 1. Find Sum of Array********************/
     public static void main(String[] args) {
 
+         //****************question 1. Find Sum of Array********************/
         int[] arr = {1, 2, 3, 4};
         int sum = 0;
 
         for (int i = 0; i < arr.length; i++) {
             sum += arr[i];
         }
-
         System.out.println("Sum = " + sum);
-        
+
+         //****************question 2. Find Maximum Element********************/
+            int[] arr2 = {10, 5, 20, 8};
+        int max = arr2[0];
+
+        for (int i = 1; i < arr2.length; i++) {
+            if (arr2[i] > max) {
+                max = arr2[i];
+            }
+        }
+
+        System.out.println("Max = " + max);
     }
+
 }
