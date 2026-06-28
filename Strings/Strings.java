52b2c78015fdb500f4be3da887e5385a7174ba40
sammiazaz <162746990+sammiazaz@users.noreply.github.com>
2026-06-28 20:18:55 +0530

    Update Strings.java

--- Strings/Strings.java
+++ Strings/Strings.java
@@ -2,7 +2,7 @@ public class Strings{
     public static void main(String[] args) {
         char[] arr = {'s','a','m','m','i'};
         for(char i =0; i < arr.length; i++){
-            System.out.println(arr);
+            System.out.println(arr); 
         }
     }
 } 
